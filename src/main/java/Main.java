8b07a8f31b195be5295bf8cd67afba6746827ea6
ana7293 <ana7293nik@gmail.com>
2023-08-01 01:42:39 +0500

    Practical work 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Guest.numGuests();
+        Product.products();
+        Product.shoplist();
+        Calculator.cheakCalcForm();
     }
 }
